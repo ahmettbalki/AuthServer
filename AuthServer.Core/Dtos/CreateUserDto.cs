d5eaab6f48a7bb91d8ba4d32feec3a618846d6b6
ahmettbalki <ahmettbalki@gmail.com>
2024-10-31 10:51:58 +0300

    added core layer

---
+++ AuthServer.Core/Dtos/CreateUserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthServer.Core.Dtos;
+public class CreateUserDto
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+}
---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using AuthServer.Core.Dtos;
+using SharedLibrary.Dtos;
+namespace AuthServer.Core.Services;
+public interface IAuthenticationService
+{
+    Task<Response<TokenDto>> CreateToken(LoginDto loginDto);
+    Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+    Task<Response<NoDataDto>> RevokeResfreshToken(string refreshToken);
+    Task<Response<ClientTokenDto>> CreateTokenByClient(ClientTokenDto clientTokenDto);
+}
